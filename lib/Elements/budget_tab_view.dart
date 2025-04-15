8c6c46ea36aa60e1ea2824827b88a2322abc0c35
Hesham Elnabtity <hesham.elnabtity@student.tugraz.at>
2025-04-15 23:27:15 +0200

    Added UI allows the user to update budget parameters or completely delete a budget

--- lib/Elements/budget_tab_view.dart
+++ lib/Elements/budget_tab_view.dart
@@ -172,6 +172,7 @@ class _BudgetTabViewState extends State<BudgetTabView> {
                                             onToggle:
                                                 () =>
                                                     _toggleCollapse(tab, b.id),
+                                            budget: b,
                                           ),
                                         );
                                       }).toList(),
