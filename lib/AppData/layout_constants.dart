25d007922894228577624f4a6dbde6b8a7b88d43
Ronald Taferner <ronald.taferner@student.tugraz.at>
2025-05-08 21:07:50 +0200

    constraints change

--- lib/AppData/layout_constants.dart
+++ lib/AppData/layout_constants.dart
@@ -4,14 +4,14 @@ class LayoutConstants {
   //static const double mainWidthProcent = 0.5;
   //static const double mainHeightProcent = 0.65;
   static const double sidebarWidthProcent = 0.2;
-  static const double contentMaxWidthProcent = 0.7;
+  static const double contentMaxWidthProcent = 0.75;
   static const double tabViewHeightProcent = 0.58;
   static const double cardWidthProcent = 0.2;
   static const double cardWidthSpacingProcent = 0.015;
   static const double spacing = 16;
 
   static const double minSidebarWidth = 200.0;
-  static const double maxSidebarWidth = 300.0;
+  static const double maxSidebarWidth = 220.0;
   static const double minContentWidth = 500.0;
   static const double minCardWidth = 220.0;
 
